61dfd5435f62d6116c88160613378943503f2db6
kai <scyjz8@nottingham.ac.uk>
2021-12-03 11:55:01 +0800

    Initial Commit

---
+++ upload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  upload
+//
+//  Created by 珏 on 2021/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
